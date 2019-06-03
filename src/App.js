2778715c1b9967ad5beacc1f5f0788b534b8b7d7
contactpeterchung.com <contactpeterchung@gmail.com>
2019-06-03 15:08:59 -0600

    Final commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import List from './List';
+import TodoForm from './TodoForm';
+import Footer from './Footer';
 
-function App() {
+class App extends Component {
+  state = { 
+  todos: [
+    { id: 1, name: 'Learn Rails', complete: true },
+    { id: 2, name: 'Learn React', complete: false },
+    { id: 3, name: 'Learn Redux', complete: false },
+  ]
+}
+
+  setFilter = (filter) => {
+    this.setState({ filter })
+  }
+
+  getUniqId = () => {
+   return Math.floor((1 + Math.random()) * 0x10000)
+     .toString(16)
+     .substring(1);
+  }
+
+addItem = (name) => {
+const { todos } = this.state;
+const todo = { name, id: this.getUniqId() , complete: false }
+this.setState({ todos: [todo, ...todos] }); 
+}
+
+handleClick = (id) => {
+  const { todos } = this.state;
+  this.setState({
+    todos: todos.map( todo => {
+      if (todo.id === id) {
+        return {
+          ...todo,
+          complete: !todo.complete
+        }
+      }
+      return todo
+    })
+  })
+}
+
+visibleItems = () => {
+  const { todos, filter } = this.state;
+  switch(filter) {
+    case 'Active':
+      return todos.filter( t => !t.complete )
+    case 'Complete':
+      return todos.filter( t=> t.complete )
+    default:
+      return todos;
+  }
+}
+
+render() {
+  const { todos, filter } = this.state;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <TodoForm addItem={this.addItem} />
+      <List name="Todo List" items={this.visibleItems()} todoClick={this.handleClick} />
+      <Footer filter={filter} setFilter={this.setFilter} />
     </div>
-  );
+   );
+  }
 }
 
 export default App;
